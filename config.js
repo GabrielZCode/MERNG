546af8bab071273092b218850e0f7fd0e422871a
GabrielZCode <86977092+GabrielZCode@users.noreply.github.com>
2022-06-15 21:29:59 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB: 'mongodb+srv://admin:ZE0Z47mde7jS0nSt@cluster0.mzcqgjx.mongodb.net/merng?retryWrites=true&w=majority',
+  TOKEN_KEY: 'rdytnd6rdnrt6iu7i8dnrn5y6nniy8er5432465576fn6u853y456nn45245ynunu4344haaddsegtf'
+
+}
